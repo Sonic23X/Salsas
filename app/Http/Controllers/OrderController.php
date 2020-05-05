d149841554d4a3d231a0334ec3bf711ef54832c4
Karen Palacios <kdodeth@gmail.com>
2020-05-05 10:27:48 -0500

    ordenes

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Entities\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class OrderController extends Controller
 {
@@ -15,6 +16,7 @@ class OrderController extends Controller
     public function index()
     {
         //
+        return view('form');
     }
 
     /**
@@ -35,7 +37,32 @@ class OrderController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->validate([
+            'code' => ['required'],
+            'store_id' => ['required'],
+            'seller_id' => ['required'],
+            'mount' => ['required'],
+            'notes' => ['required'],
+            'salsa_id' => ['required'], 
+            'quantity' => ['required'],
+            'price' => ['required']
+        ]);
+            
+        $order = Order::create([
+            'code' => $data['code'],
+            'store_id' => $data['store_id'],
+            'seller_id' => $data['seller_id'],
+            'mount' => $data['mount'],
+            'notes' => $data['notes']
+        ]);
+        
+        $id = Order::max('id');
+        
+        $detail= Order::find($id);
+
+        $detail->salsas()->attach($id,['salsa_id'=>$data['salsa_id'],'quantity'=>$data['quantity'], 'price'=>$data['price']]);
+        
+        return response()->json($order);
     }
 
     /**
@@ -69,7 +96,28 @@ class OrderController extends Controller
      */
     public function update(Request $request, Order $order)
     {
-        //
+        $data = $request->validate([
+            'code' => ['required'],
+            'store_id' => ['required'],
+            'seller_id' => ['required'],
+            'mount' => ['required'],
+            'notes' => ['required'],
+            'salsa_id' => ['required'], 
+            'quantity' => ['required'],
+            'price' => ['required']
+        ]);
+        
+        $orders = Order::find($order);
+        $orders->code = $data['code'];
+        $orders->store_id = $data['store_id'];
+        $orders->seller_id = $data['seller_id'];
+        $orders->mount = $data['mount'];
+        $orders->notes = $data['notes'];
+        $orders->save();
+
+        $orders->salsas()->updateExistingPivot($order,['salsa_id'=>$data['salsa_id'],'quantity'=>$data['quantity'], 'price'=>$data['price']]);
+        
+        return response()->json($orders);
     }
 
     /**
@@ -80,6 +128,8 @@ class OrderController extends Controller
      */
     public function destroy(Order $order)
     {
-        //
+        $orders = Order::find($order);
+        $orders->delete();
+        return response()->json($orders);
     }
 }
