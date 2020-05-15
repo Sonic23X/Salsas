3b7f47a09a88bbc5c563767108aed59503b62e71
Sam Cruz <30601547+Sammelot@users.noreply.github.com>
2020-05-14 19:20:43 -0500

    fixing merge issue

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -16,7 +16,6 @@ class OrderController extends Controller
     public function index()
     {
         //
-        return view('form');
     }
 
     /**
@@ -43,11 +42,11 @@ class OrderController extends Controller
             'seller_id' => ['required'],
             'mount' => ['required'],
             'notes' => ['required'],
-            'salsa_id' => ['required'], 
+            'salsa_id' => ['required'],
             'quantity' => ['required'],
             'price' => ['required']
         ]);
-            
+
         $order = Order::create([
             'code' => $data['code'],
             'store_id' => $data['store_id'],
@@ -55,13 +54,13 @@ class OrderController extends Controller
             'mount' => $data['mount'],
             'notes' => $data['notes']
         ]);
-        
+
         $id = Order::max('id');
-        
+
         $detail= Order::find($id);
 
         $detail->salsas()->attach($id,['salsa_id'=>$data['salsa_id'],'quantity'=>$data['quantity'], 'price'=>$data['price']]);
-        
+
         return response()->json($order);
     }
 
@@ -102,11 +101,11 @@ class OrderController extends Controller
             'seller_id' => ['required'],
             'mount' => ['required'],
             'notes' => ['required'],
-            'salsa_id' => ['required'], 
+            'salsa_id' => ['required'],
             'quantity' => ['required'],
             'price' => ['required']
         ]);
-        
+
         $orders = Order::find($order);
         $orders->code = $data['code'];
         $orders->store_id = $data['store_id'];
@@ -116,7 +115,7 @@ class OrderController extends Controller
         $orders->save();
 
         $orders->salsas()->updateExistingPivot($order,['salsa_id'=>$data['salsa_id'],'quantity'=>$data['quantity'], 'price'=>$data['price']]);
-        
+
         return response()->json($orders);
     }
 
